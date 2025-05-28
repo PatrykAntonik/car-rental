12c0e4482ab688724c61d6b02d714e7f342767a7
Patryk <patryka2000@gmail.com>
2025-05-29 00:31:16 +0200

    toggle db type

--- car_rental/settings.py
+++ car_rental/settings.py
@@ -135,28 +135,27 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'car_rental.wsgi.application'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['DB_NAME'],
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': os.environ['DB_HOST'],
-        'PORT': '5432',
-        'OPTIONS': {
-            'sslmode': 'require',
+if os.getenv("USE_SQLITE", "False").lower() == "true":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ['DB_NAME'],
+            'USER': os.environ['DB_USER'],
+            'PASSWORD': os.environ['DB_PASSWORD'],
+            'HOST': os.environ['DB_HOST'],
+            'PORT': '5432',
+            'OPTIONS': {
+                'sslmode': 'require',
+            }
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
