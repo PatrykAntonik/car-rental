7e34fd7353dbfd3ff41ee34e5430bfa83d3e1b2f
Patryk <patryka2000@gmail.com>
2025-05-28 15:56:38 +0200

    migrated to postgres

--- car_rental/settings.py
+++ car_rental/settings.py
@@ -135,10 +135,25 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'car_rental.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require',
+        }
     }
 }
 
